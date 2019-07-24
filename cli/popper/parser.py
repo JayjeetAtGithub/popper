af9481a45a0db541dc1ceb44e212cb444182f04d
Jayjeet Chakraborty <jc.github@rediffmail.com>
2019-07-24 20:59:08 +0530

    Refactored parser.py

--- cli/popper/parser.py
+++ cli/popper/parser.py
@@ -7,9 +7,84 @@ import hcl
 from popper.cli import log
 from popper import utils as pu
 
-# The valid Workflow and Action attributes.
+
 VALID_ACTION_ATTRS = ["uses", "args", "needs", "runs", "secrets", "env"]
 VALID_WORKFLOW_ATTRS = ["resolves", "on"]
+
+
+def list_of_strings(arr):
+    """Utility function to check whether a list consists of only
+    strings or not.
+
+    Args:
+        arr (list) : The list to verify.
+
+    Returns:
+        bool : Whether the list consists of only strings or not.
+    """
+    # Python 2 to 3 Compability
+    try:
+        basestring
+    except UnboundLocalError:
+        basestring = str
+    return bool(arr) and isinstance(arr, list) and all(
+        isinstance(elem, basestring) for elem in arr)
+
+
+class Action(object):
+    """Represent's an Action."""
+
+    def __init__(self, name, **attrs):
+        self.attrs = attrs
+        self.name = name
+        self.uses = attrs.get('uses', None)
+        self.needs = attrs.get('needs', None)
+        self.needs = list(self.needs) if self.needs else self.needs
+        self.args = self._normalize(attrs.get('args', None))
+        self.runs = self._normalize(attrs.get('runs', None))
+        self.env = attrs.get('env', None)
+        self.secrets = self._normalize(attrs.get('secrets', None))
+        self.next = None
+        
+        self._validate_action_block()
+
+    def _normalize(self, params):
+        if not params:
+            return None
+        if isinstance(params, str):
+            return params.split(" ")
+        elif isinstance(params, list):
+            return " ".join(params).split(" ")
+        else:
+            return params
+
+    def _validate_action_block(self):
+        """Validate the action attributes."""
+        for key in self.attrs.keys():
+            if key not in VALID_ACTION_ATTRS:
+                log.fail(
+                    'Invalid action attribute \'{}\' was found.'.format(key))
+
+        if not self.uses:
+            log.fail("[uses] attribute must be present.")
+
+        if not isinstance(self.uses, str):
+            log.fail("[uses] attribute must be a string.")
+
+        if self.needs and not list_of_strings(self.needs):
+            log.fail("[needs] attribute must be a string or list.")
+
+        if self.runs and not list_of_strings(self.runs):
+            log.fail("[runs] attribute must be a string or a list.")
+        
+        if self.args and not list_of_strings(self.args):
+            log.fail("[args] attribute must be a string or a list.")
+
+        if self.env and not isinstance(self.env, dict):
+            log.fail("[env] attribute must be a dict.")
+
+        if self.secrets and not list_of_strings(self.secrets):
+            log.fail("[secrets] attribute must be a string or a list.")
 
 
 class Workflow(object):
@@ -18,57 +93,94 @@ class Workflow(object):
 
     def __init__(self, wfile):
 
-        # Read's the workflow file.
         with open(wfile, 'r') as fp:
-            self._workflow = hcl.load(fp)
+            self._parsed_wfile = hcl.load(fp)
             fp.seek(0)
-            self._workflow_content = fp.readlines()
+            self._wfile_content = fp.readlines()
+        
+        # Check the workflow file for correct composition
+        # of actions and workflow blocks.
+        name, attrs = self._validate_wfile(self._parsed_wfile)
 
-        self._validate_syntax()
-        self._normalize()
-        self._check_for_empty_workflow()
+        self.actions = dict()
+        self.root = None
+        self.name = name
+        self.attrs = attrs
+        self.on = attrs.get('on', 'push')
+        self.resolves = attrs.get('resolves', None)
+        self.resolves = list(self.resolves) if self.resolves else None
+
+        self._validate_workflow_block()
+        self._wrap_actions(self._parsed_wfile.get('action', None))
         self._complete_graph()
 
-    @property
-    def name(self):
-        """The name of the workflow."""
-        return self._workflow['name']
+    ## Parent function to call all the workflow file validators.
+    def _validate_wfile(self, parsed_wfile):
+        """Validate the workflow file in 3 stages:
+            * Check for multiple workflow blocks.
+            * Check for duplicate action blocks.
+        """
+        self._check_multiple_workflow_blocks()
+        self._check_duplicate_actions()
+        return list(parsed_wfile['workflow'].items())[0]
 
-    @property
-    def on(self):
-        """The value of `on` attribute."""
-        return self._workflow['on']
+    ## Functions for validating the entire `.workflow` file.
+    def _check_multiple_workflow_blocks(self):
+        """Checks whether the workflow file contains more than
+        one workflow blocks. If True, it throws error."""
+        wf_count = len(self._parsed_wfile.get('workflow', dict()).items())
+        if wf_count != 1:
+            log.fail('Only a single workflow block is allowed per workflow file.')
 
-    @property
-    def props(self):
-        return self._workflow['props']
+    def _check_for_empty_workflow(self):
+        """Checks whether the actions that the workflow wants to resolve
+        exists or not."""
+        actions = set(map(lambda a: a[0], self._parsed_wfile['action'].items()))
+        if not set(self.attrs['resolves']).intersection(actions):
+            log.fail('Can\'t resolve any of the actions.')
 
-    @property
-    def root(self):
-        """The value of the `root` attribute."""
-        return self._workflow['root']
+    def _check_duplicate_actions(self):
+        """Checks whether duplicate action blocks are
+        present or not."""
+        parsed_acount = 0
+        if self._parsed_wfile.get('action', None):
+            parsed_acount = len(list(self._parsed_wfile['action'].items()))
+        acount = 0
+        for line in self._wfile_content:
+            line = line.strip()
+            if line.startswith('action '):
+                acount += 1
+        if parsed_acount != acount:
+            log.fail('Duplicate action identifiers found.')
 
-    @property
-    def resolves(self):
-        """The value of the `resolves` attribute."""
-        return self._workflow['resolves']
+    ## Function to validate the attributes of the workflow block.
+    def _validate_workflow_block(self):
+        """Validate the workflow attributes."""
+        for key in self.attrs.keys():
+            if key not in VALID_WORKFLOW_ATTRS:
+                log.fail(
+                    'Invalid workflow attribute \'{}\' was found.'.format(key))
 
-    @property
-    def actions(self):
-        """The list of actions in a workflow."""
-        return self._workflow['action']
+        if not self.resolves:
+            log.fail('[resolves] attribute must be present.')
 
-    def get_runner(self, action):
-        """Returns the runner required to run an action."""
-        return self._workflow['action'][action]['runner']
+        if not list_of_strings(self.resolves):
+            log.fail('[resolves] attribute must be a string or a list.')
 
-    def get_action(self, action):
-        """Returns an action from a workflow."""
-        if self._workflow['action'].get(action, None):
-            return self._workflow['action'][action]
-        else:
-            log.fail("Action '{}' doesn\'t exist.".format(action))
+        if not isinstance(self.on, str):
+            log.fail('[on] attribute must be a string or a list.')
 
+    ## Function to wrap the actions along with other metadata
+    def _wrap_actions(self, actions_dict):
+        """Validate the action part of the worflow and 
+        initialize Action objects."""
+        if not actions_dict:
+            log.fail('At least one action block must be present.')
+
+        for name, attrs in actions_dict.items():
+            action = Action(name, **attrs)
+            self.actions[name] = action
+            
     @pu.threadsafe_generator
     def get_stages(self):
         """Generator of stages. A stages is a list of actions that can be
@@ -85,11 +197,6 @@ class Workflow(object):
                         'next', set()))
             current_stage = next_stage
 
-    def _check_for_empty_workflow(self):
-        actions = set(map(lambda a: a[0], self._workflow['action'].items()))
-        if not set(self._workflow['resolves']).intersection(actions):
-            log.fail('Can\'t resolve any of the actions.')
-
     def _complete_graph_util(self, entrypoint, nwd):
         """A GHA workflow is defined by specifying edges that point to the
         previous nodes they depend on. To make the workflow easier to process,
@@ -101,13 +208,13 @@ class Workflow(object):
             nwd (set) : Set of nodes without dependencies.
         """
         for node in entrypoint:
-            if self._workflow['action'].get(node, None):
-                if self._workflow['action'][node].get('needs', None):
-                    for n in self._workflow['action'][node]['needs']:
+            if self.actions.get(node, None):
+                if self.actions[node].needs:
+                    for n in self.actions.node.needs:
                         self._complete_graph_util([n], nwd)
-                        if not self._workflow['action'][n].get('next', None):
-                            self._workflow['action'][n]['next'] = set()
-                        self._workflow['action'][n]['next'].add(node)
+                        if not self.actions[n].next:
+                            self.actions[n].next = set()
+                        self.actions[n].next.add(node)
                 else:
                     nwd.add(node)
             else:
@@ -117,149 +224,10 @@ class Workflow(object):
         """Driver function to run the recursive function
         `_complete_graph_util()` which adds forward edges.
         """
-        nwd = set()
-        self._complete_graph_util(self._workflow['resolves'], nwd)
-        self._workflow['root'] = nwd
-
-    @staticmethod
-    def _is_list_of_strings(arr):
-        """Utility function to check whether a list consists of only
-        strings or not.
-
-        Args:
-            arr (list) : The list to verify.
-
-        Returns:
-            bool : Whether the list consists of only strings or not.
-        """
-        # Python 2 to 3 Compability
-        try:
-            basestring
-        except UnboundLocalError:
-            basestring = str
-        return bool(arr) and isinstance(arr, list) and all(
-            isinstance(elem, basestring) for elem in arr)
-
-    def _validate_syntax(self):
-        """ Validates the `.workflow` file by checking whether required items
-        are specified, and if extra attributes not defined in the GHA
-        specification are part of a workflow."""
-
-        # Validates the workflow block
-        if not self._workflow.get('workflow', None):
-            log.fail('A workflow block must be present.')
-        elif len(self._workflow['workflow'].items()) > 1:
-            log.fail('Cannot have more than one workflow blocks.')
-        else:
-            wf_block = list(self._workflow['workflow'].values())[0]
-            for key in wf_block.keys():
-                if key not in VALID_WORKFLOW_ATTRS:
-                    log.fail(
-                        'Invalid workflow attribute \'{}\' was found.'.format(key))
-            if not wf_block.get('resolves', None):
-                log.fail('[resolves] attribute must be present.')
-
-        # Validates the action blocks
-        self._check_duplicate_actions()
-
-        if not self._workflow.get('action', None):
-            log.fail('Atleast one action block must be present.')
-        else:
-            for _, a_block in self._workflow['action'].items():
-                for key in a_block.keys():
-                    if key not in VALID_ACTION_ATTRS:
-                        log.fail(
-                            'Invalid action attribute \'{}\' was '
-                            'found.'.format(key))
-                if not a_block.get('uses', None):
-                    log.fail('[uses] attribute must be present.')
-
-    def _normalize(self):
-        """Normalize the dictionary representation of the workflow by creating
-        lists for all attributes that can be either a string or a list."""
-        # move from this:
-        #
-        #  "workflow": {
-        #    "test-and-deploy": {
-        #      "resolves": "deploy"
-        #    }
-        #  }
-        #
-        # to this (top-level items in workflow dictionary):
-        #
-        #  "name": "test-and-deploy",
-        #  "on": "push",
-        #  "resolves": "deploy"
-        #
-
-        for wf_name, wf_block in dict(self._workflow['workflow']).items():
-            self._workflow['name'] = wf_name
-            self._workflow['on'] = wf_block.get('on', 'push')
-            self._workflow['props'] = dict()
-            self._workflow['resolves'] = wf_block['resolves']
-
-        del (self._workflow['workflow'])
-
-        # Python 2 to 3 Compability
-        try:
-            basestring
-        except UnboundLocalError:
-            basestring = str
-
-        # Create a list for all attributes that can be either string or list
-        if isinstance(self._workflow['resolves'], basestring):
-            self._workflow['resolves'] = [self._workflow['resolves']]
-        elif not self._is_list_of_strings(self._workflow['resolves']):
-            log.fail('[resolves] must be a list of strings or a string')
-        if not isinstance(self._workflow['on'], basestring):
-            log.fail('[on] attribute must be a string')
-        for a_name, a_block in self._workflow['action'].items():
-
-            a_block['name'] = a_name
-
-            if not isinstance(a_block['uses'], basestring):
-                log.fail('[uses] attribute must be a string')
-            if a_block.get('needs', None):
-                if isinstance(a_block['needs'], basestring):
-                    a_block['needs'] = [a_block['needs']]
-                elif not self._is_list_of_strings(a_block['needs']):
-                    log.fail(
-                        '[needs] attribute must be a list of strings '
-                        'or a string')
-            if a_block.get('runs', None):
-                if isinstance(a_block['runs'], basestring):
-                    a_block['runs'] = [a_block['runs']]
-                elif not self._is_list_of_strings(a_block['runs']):
-                    log.fail(
-                        '[runs] attribute must be a list of strings '
-                        'or a string')
-            if a_block.get('args', None):
-                if isinstance(a_block['args'], basestring):
-                    a_block['args'] = a_block['args'].split()
-                elif not self._is_list_of_strings(a_block['args']):
-                    log.fail(
-                        '[args] attribute must be a list of strings '
-                        'or a string')
-            if a_block.get('env', None):
-                if not isinstance(a_block['env'], dict):
-                    log.fail('[env] attribute must be a dict')
-            if a_block.get('secrets', None):
-                if not self._is_list_of_strings(a_block['secrets']):
-                    log.fail('[secrets] attribute must be a list of strings')
-
-    def _check_duplicate_actions(self):
-        """Checks whether duplicate action blocks are
-        present or not."""
-        parsed_acount = 0
-        if self._workflow.get('action', None):
-            parsed_acount = len(list(self._workflow['action'].items()))
-        acount = 0
-        for line in self._workflow_content:
-            line = line.strip()
-            if line.startswith('action '):
-                acount += 1
-        if parsed_acount != acount:
-            log.fail('Duplicate action identifiers found.')
+        self._check_for_empty_workflow()
+        nodes_without_dependencies = set()
+        self._complete_graph_util(self.resolves, nodes_without_dependencies)
+        self.root = nwd
 
     def check_for_unreachable_actions(self, skip=None):
         """Validates a workflow by checking for unreachable nodes / gaps
@@ -296,7 +264,7 @@ class Workflow(object):
             self._workflow['action'].pop(a)
 
     @staticmethod
-    def skip_actions(wf, skip_list):
+    def skip_actions(wf, skip_list=list()):
         """Removes the actions to be skipped from the workflow graph and
         return a new `Workflow` object.
 
@@ -309,7 +277,11 @@ class Workflow(object):
         """
         workflow = deepcopy(wf)
         for sa_name in skip_list:
+
+            # Isolate the skipped actions totally.
             sa_block = workflow.get_action(sa_name)
+            sa_block['next'].clear()
+            sa_block['needs'].clear()
 
             # Handle skipping of root action's
             if sa_name in workflow.root:
@@ -317,16 +289,18 @@ class Workflow(object):
 
             # Handle skipping of not-root action's
             for a_name, a_block in workflow.actions.items():
+
                 if sa_name in a_block.get('next', set()):
                     a_block['next'].remove(sa_name)
-                    if a_name in sa_block.get('needs', []):
-                        sa_block['needs'].remove(a_name)
+                
+                if sa_name in a_block.get('needs', list()):
+                    a_block['needs'].remove(sa_name)
 
         workflow.props['skip_list'] = list(skip_list)
         return workflow
 
     @staticmethod
-    def filter_action(wf, action, with_dependencies):
+    def filter_action(wf, action, with_dependencies=False):
         """Filters out all actions except the one passed in
         the argument from the workflow.
 
